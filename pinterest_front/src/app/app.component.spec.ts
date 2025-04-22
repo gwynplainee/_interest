4953ca5f4c825d88451c0684409f78f2d2c887fa
Kaisar I <imangalikaisar05@mail.ru>
2025-04-22 20:17:37 +0500

    file change - created new front folder

--- pinterest_front/src/app/app.component.spec.ts
+++ pinterest_front/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'pinterest-clone' title`, () => {
+  it(`should have the 'pinterest_front' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pinterest-clone');
+    expect(app.title).toEqual('pinterest_front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pinterest-clone');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pinterest_front');
   });
 });
--- pinterest_front/src/app/app.component.ts
+++ pinterest_front/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pinterest-clone';
+  title = 'pinterest_front';
 }
